4228781cf9853e658972cc7a8eccf551e999d2e7
Andy Armstrong <andy@hexten.net>
2026-01-04 14:39:26 +0000

    Investigate build tardiness

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,28 @@
-pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const gpa = arena.allocator();
-    var args = std.process.args();
-    _ = args.skip();
+const std = @import("std");
 
-    while (args.next()) |arg| {
-        std.debug.print("Loading {s}\n", .{arg});
-        const src = try std.fs.cwd().readFileAlloc(arg, gpa, .unlimited);
-        defer gpa.free(src);
-        std.debug.print("Loaded 0x{x} bytes\n", .{src.len});
+const IbexFloat = @import("./ibex//IbexFloat.zig").IbexFloat;
+const ibex = @import("./ibex/ibex.zig");
+const ByteWriter = ibex.ByteWriter;
+const ByteReader = ibex.ByteReader;
+
+pub fn main() !void {
+    std.debug.print("Woof\n", .{});
+    const types = [_]type{ u8, i9, i13, i32, u33, u32, u64, u1024, i1024, u32768 };
+    inline for (types) |T| {
+        // std.debug.print("=== {any} ===\n", .{T});
+        const IF = IbexFloat(T);
+        const values = [_]T{ 0, std.math.minInt(T), std.math.maxInt(T) };
+        for (values) |value| {
+            var buf: [32768 / 7 + 256]u8 = undefined;
+            var w = ByteWriter{ .buf = &buf };
+            try IF.write(&w, value);
+            std.debug.print("{any}\n", .{w.slice()});
+            var r = ByteReader{ .buf = w.slice() };
+            const got = try IF.read(&r);
+            std.debug.print("got {any}\n", .{got == value});
+        }
     }
 }
-
-const std = @import("std");
-const assert = std.debug.assert;
 
 test {
     _ = @import("./tree.zig");
