a15e84001dc3ceef3ba44f111f7e18d6fe9b531d
Andy Armstrong <andy@hexten.net>
2026-01-09 03:13:42 +0000

    Baseline

---
+++ bin/bm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+hash=$(git rev-parse --short HEAD)
+host=$(hostname -s)
+bmfile="ref/bm-$host.txt"
+
+zig build -Doptimize=ReleaseFast
+echo "$( date ) - $hash - $host" >> $bmfile
+zig-out/bin/bm-numbers | tee -a $bmfile
+
+# vim:ts=2:sw=2:sts=2:et:ft=sh
+
