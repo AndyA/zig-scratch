86f6cf8e3529512f4a3bac97db434e7316ed24f7
Andy Armstrong <andy@hexten.net>
2026-01-09 03:44:56 +0000

    Use ISO time

--- bin/bm.sh
+++ bin/bm.sh
@@ -4,10 +4,11 @@ set -e
 
 hash=$(git rev-parse --short HEAD)
 host=$(hostname -s)
+stamp=$(date -Iseconds)
 bmfile="ref/bm-$host.txt"
 
 zig build -Doptimize=ReleaseFast
-echo "$( date ) - $hash - $host" >> $bmfile
+echo "$stamp - $hash - $host" | tee -a $bmfile
 zig-out/bin/bm-numbers 2>&1 | tee -a $bmfile
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
