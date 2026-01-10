9995ad0bd484f4432a99bff3c14ec521c8066433
Andy Armstrong <andy@hexten.net>
2026-01-10 18:24:04 +0000

    Add zig version

--- bin/bm.sh
+++ bin/bm.sh
@@ -3,12 +3,13 @@
 set -e
 
 hash=$(git rev-parse --short HEAD)
+zig=$(zig version)
 host=$(hostname -s)
 stamp=$(date -Iseconds)
 bmfile="ref/bm-$host.txt"
 
 zig build -Doptimize=ReleaseFast
-echo "$stamp - $hash - $host" | tee -a $bmfile
+echo "$stamp - $hash - $host - $zig" | tee -a $bmfile
 zig-out/bin/bm-numbers 2>&1 | tee -a $bmfile
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
