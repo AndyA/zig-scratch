7c1b5b5c8b89a3b188870d32f39278d98af3d134
Andy Armstrong <andy@hexten.net>
2026-01-12 21:39:32 +0000

    Minor

--- src/bm-codec.zig
+++ src/bm-codec.zig
@@ -8,14 +8,14 @@ const IbexNumber = @import("./ibex/IbexNumber.zig").IbexNumber;
 const IbexInt = @import("./ibex/IbexInt.zig");
 
 const Benchmarks = struct {
-    pub fn @"IbexNumber/f64"(io: std.Io, gpa: Allocator, name: []const u8) !void {
+    pub fn @"IbexNumber/f64"(io: std.Io, gpa: Allocator, comptime name: []const u8) !void {
         const numbers = try bm.loadTestData(f64, io, gpa, "ref/testdata/f64sample.bin");
         defer gpa.free(numbers);
         const codec = IbexNumber(f64);
         try bm.benchmarkCodec(gpa, codec, numbers, .{ .repeats = 1000, .name = name });
     }
 
-    pub fn @"IbexInt/lengths"(io: std.Io, gpa: Allocator, name: []const u8) !void {
+    pub fn @"IbexInt/lengths"(io: std.Io, gpa: Allocator, comptime name: []const u8) !void {
         const numbers = try bm.loadTestData(i64, io, gpa, "ref/testdata/i64lengths.bin");
         defer gpa.free(numbers);
         try bm.benchmarkCodec(gpa, IbexInt, numbers, .{ .repeats = 5000, .name = name });
